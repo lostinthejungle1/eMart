50cb73b16b26a8d1ac9f88604676475e0979dceb
Liang <lianginchina1999@gmail.com>
2024-06-16 15:10:52 +0800

    create guide page before login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import './style/app.css';
 
 function App() {
+  const refGuidePage = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const guidePage = refGuidePage.current;
+    guidePage?.classList.add('show');
+  },[]);
   return (
-    <div >
-      test
+    <div ref={refGuidePage} className='app guide-page' style={{fontSize:'.2rem'}}>
+      <img src={require('./images/emart_logo_icon_@2x.png')} alt='logo' className='logo' />
+      <h1 className='title'>Emart</h1>
+      <img src={require('./images/slogan@2x.png')} alt='slogan' className='slogan' />
+      <img src={require('./images/next_step_icon_@2x.png')} alt='next' className='next' />
     </div>
   );
 }
