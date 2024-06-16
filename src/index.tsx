88c49afba82e8c8c00f84728100f1e92f8b07a0d
Liang <lianginchina1999@gmail.com>
2024-06-16 15:11:10 +0800

    add initial styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './style/border.css';
+// import 'normalize.css'
+import './style/base.css'
+
+document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px';
+window.addEventListener('resize', () => {
+  document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px';
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
