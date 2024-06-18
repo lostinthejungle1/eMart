94f76f14a6293175cd72236c68d4fb5035dcfc67
Liang <lianginchina1999@gmail.com>
2024-06-18 21:44:08 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import './style/border.css';
-// import 'normalize.css'
-import './style/base.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px';
+document.documentElement.style.fontSize =
+    (document.documentElement.clientWidth / 375) * 100 + 'px'
 window.addEventListener('resize', () => {
-  document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 * 100 + 'px';
-});
+    document.documentElement.style.fontSize =
+        (document.documentElement.clientWidth / 375) * 100 + 'px'
+})
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
