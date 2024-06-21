313c4e92279d636e8a6391739737885e8c3957fe
Liang <lianginchina1999@gmail.com>
2024-06-21 15:10:50 +0800

    add mine page

---
+++ src/containers/Mine/index.tsx
@@ -0,0 +1,12 @@
+import NavigationBar from '../../components/NavigationBar'
+import './style.css'
+function Mine() {
+    return (
+        <div className="mine-page">
+            mine
+            <NavigationBar current="mine" className="nav-bar" />
+        </div>
+    )
+}
+
+export default Mine
