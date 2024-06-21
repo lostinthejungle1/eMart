cb5fecc03aef00171eef6edd460d3b1986c63568
Liang <lianginchina1999@gmail.com>
2024-06-21 15:07:50 +0800

    add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+function Header({ title }: { title: string }) {
+    return (
+        <div className="header">
+            <h2>{title}</h2>
+        </div>
+    )
+}
+
+export default Header
