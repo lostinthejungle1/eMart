7fbc6b1316bcc667a82041bf4a633382ffd79c03
Liang <lianginchina1999@gmail.com>
2024-06-21 15:13:10 +0800

    add cart page

---
+++ src/containers/Cart/index.tsx
@@ -0,0 +1,12 @@
+import NavigationBar from '../../components/NavigationBar'
+import './style.css'
+function Cart() {
+    return (
+        <div className="cart-page">
+            cart
+            <NavigationBar current="cart" className="nav-bar" />
+        </div>
+    )
+}
+
+export default Cart
