f05eca64af29ad212d7e097dad7a06b6b923c503
Liang <lianginchina1999@gmail.com>
2024-06-23 13:54:01 +0800

    add purchase popup content

--- src/containers/Category/index.tsx
+++ src/containers/Category/index.tsx
@@ -7,6 +7,9 @@ import axios from 'axios'
 import { getToken } from '../../utils'
 import { Toast } from 'antd-mobile'
 import Product from './Product'
+import { useNavigate } from 'react-router-dom'
+import Popover from '../../components/Popover'
+import { set } from 'lodash'
 type category = {
     name: string
     display_name: string
@@ -30,7 +33,9 @@ function Category() {
     const [category, setCategory] = useState<Array<category>>([])
     const [selectedCategory, setSelectedCategory] = useState('all')
     const [selectedPrice, setSelectedPrice] = useState('')
-
+    const [selectedProduct, setSelectedProduct] = useState<product | null>(null)
+    const [cartNumber, setCartNumber] = useState(0)
+    const navigate = useNavigate()
     useEffect(() => {
         axios
             .get(process.env.REACT_APP_API_URL + '/categories', {
@@ -64,6 +69,49 @@ function Category() {
                 console.log(err)
             })
     }, [selectedCategory])
+    const openProductDetail = (id: number) => {
+        navigate('/detail/' + id)
+    }
+    const handlePurchase = (product: product) => {
+        setSelectedProduct(product)
+    }
+
+    const addToCart = (id: number, count: number) => {
+        axios
+            .post(
+                process.env.REACT_APP_API_URL + '/cart-add',
+                {
+                    product_id: id,
+                    count: count,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${getToken()}`,
+                    },
+                }
+            )
+            .then((res) => {
+                Toast.show({
+                    icon: 'success',
+                    content: '添加购物车成功',
+                })
+                setSelectedProduct(null)
+                setCartNumber(0)
+                navigate('/cart')
+            })
+            .catch((err) => {
+                console.log(err)
+                Toast.show({
+                    icon: 'fail',
+                    content: '添加购物车失败',
+                })
+            })
+    }
+
+    const makeOrder = (id: number, count: number) => {
+        console.log('make order')
+    }
+
     return (
         <div className="category-page">
             <Header title="分类" />
@@ -106,15 +154,78 @@ function Category() {
                         {products.map((product) => (
                             <Product
                                 key={product.id}
-                                product_id={product.id}
-                                thumbnail_url={product.thumbnail_url}
-                                sales_count={product.sales_count}
-                                price={product.price}
-                                title={product.title}
+                                product={product}
+                                openProductDetail={openProductDetail}
+                                handlePurchase={handlePurchase}
                             />
                         ))}
                     </div>
                 </div>
+                <Popover
+                    show={selectedProduct !== null}
+                    blankClickCallback={() => {
+                        setSelectedProduct(null)
+                    }}
+                >
+                    <div className="pop-content">
+                        <div className="brief-block">
+                            <img
+                                src={
+                                    'https://mbg.com.my/cdn/shop/files/USA-Strawberry-250G-Berries_1024x1024.jpg?v=1714413327'
+                                }
+                                alt={selectedProduct?.title}
+                                className="pop-thumbnail"
+                            />
+                            <h3 className="pop-title">
+                                {selectedProduct?.title}
+                            </h3>
+                            <span className="pop-price">
+                                <span style={{ fontSize: '.14rem' }}>￥</span>
+                                {selectedProduct?.price}
+                            </span>
+                        </div>
+                        <div className="number-row">
+                            <span>购买数量</span>
+                            <div className="number-input">
+                                <span
+                                    onClick={() => {
+                                        if (cartNumber > 0) {
+                                            setCartNumber((prev) => prev - 1)
+                                        }
+                                    }}
+                                >
+                                    -
+                                </span>
+                                <span>{cartNumber}</span>
+                                <span
+                                    onClick={() =>
+                                        setCartNumber((prev) => prev + 1)
+                                    }
+                                >
+                                    +
+                                </span>
+                            </div>
+                        </div>
+                        <div className="btns">
+                            <button
+                                className="add-btn"
+                                onClick={() =>
+                                    addToCart(selectedProduct?.id!, cartNumber)
+                                }
+                            >
+                                加入购物车
+                            </button>
+                            <button
+                                className="purchase-btn"
+                                onClick={() =>
+                                    makeOrder(selectedProduct?.id!, cartNumber)
+                                }
+                            >
+                                立即购买
+                            </button>
+                        </div>
+                    </div>
+                </Popover>
             </div>
             <NavigationBar current="category" className="nav-bar" />
         </div>
