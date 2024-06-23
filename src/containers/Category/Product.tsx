50d265b8468dee9b93b9f3ff60e6afee904b3882
Liang <lianginchina1999@gmail.com>
2024-06-23 13:53:42 +0800

    chang input type

--- src/containers/Category/Product.tsx
+++ src/containers/Category/Product.tsx
@@ -1,28 +1,46 @@
-function Product({
-    product_id,
-    thumbnail_url,
-    sales_count,
-    price,
-    title,
-}: {
-    product_id: number
-    thumbnail_url: string
-    sales_count: number
+type product = {
+    id: number
     price: number
+    sales_count: number
     title: string
+    description: string
+    specifications: string
+    category: string
+    thumbnail_url: string
+}
+
+function Product({
+    product,
+    openProductDetail,
+    handlePurchase,
+}: {
+    product: product
+    openProductDetail: (id: number) => void
+    handlePurchase: (product: product) => void
 }) {
     return (
-        <div className="product-container">
+        <div
+            className="product-container"
+            onClick={() => openProductDetail(product.id)}
+        >
             <img
                 className="product-img"
                 src="https://c8.alamy.com/comp/CF9DGC/strawberry-CF9DGC.jpg"
-                alt={title}
+                alt={product.title}
             />
             <div className="product-info">
-                <div className="product-title">{title}</div>
-                <div className="product-sales">销量:{sales_count}</div>
-                <div className="product-price">{price}</div>
-                <button className="purchase-btn">购买</button>
+                <div className="product-title">{product.title}</div>
+                <div className="product-sales">销量:{product.sales_count}</div>
+                <div className="product-price">{product.price}</div>
+                <button
+                    className="product-purchase-btn"
+                    onClick={(e) => {
+                        e.stopPropagation()
+                        handlePurchase(product)
+                    }}
+                >
+                    购买
+                </button>
             </div>
         </div>
     )
