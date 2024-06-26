e1104036168d3ca922655b2ea8c80f3ac1a4ab1a
Liang <lianginchina1999@gmail.com>
2024-06-26 23:09:29 +0800

    add path prop for returnBtn

--- src/components/ReturnBtn/index.tsx
+++ src/components/ReturnBtn/index.tsx
@@ -2,14 +2,14 @@ import { IoIosArrowBack } from 'react-icons/io'
 import { useNavigate } from 'react-router-dom'
 import './style.css'
 
-function ReturnBtn() {
+function ReturnBtn({ path }: { path: string }) {
     const navigate = useNavigate()
     return (
         <>
             <IoIosArrowBack
                 className="back-btn"
                 onClick={() => {
-                    navigate(-1)
+                    navigate(path)
                 }}
             />
         </>
