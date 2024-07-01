0e3d6030c61d50e3b8daac2a8a257a0a4b9aa331
Liang <lianginchina1999@gmail.com>
2024-07-01 23:49:14 +0800

    create orderproduct component

---
+++ src/components/OrderProduct/index.tsx
@@ -0,0 +1,30 @@
+import './style.css'
+type product = {
+    product_id: number
+    title: string
+    price: number
+    thumbnail_url: string
+    count: number
+    spec: string
+}
+function OrderProduct({ product }: { product: product }) {
+    return (
+        <div className="order-product-container">
+            <img
+                src="https://c8.alamy.com/comp/CF9DGC/strawberry-CF9DGC.jpg"
+                alt={product.title}
+                className="product-thumbnail"
+            />
+            <div className="product-info">
+                <div className="product-title">{product.title}</div>
+                <div className="product-spec">{product.spec}</div>
+                <div className="product-figure">
+                    <span className="product-price">{product.price}</span>
+                    <span className="product-count">{product.count}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OrderProduct
