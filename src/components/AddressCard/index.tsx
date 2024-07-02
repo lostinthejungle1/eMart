e26f853dbae5eaaa4f9a1ce1507ed5079a21c2d6
Liang <lianginchina1999@gmail.com>
2024-07-02 17:23:42 +0800

    update default place sign of the address card component

--- src/components/AddressCard/index.tsx
+++ src/components/AddressCard/index.tsx
@@ -12,44 +12,44 @@ function AddressCard({
     address,
     deleteAddress,
     showBtns,
+    onClickHandler,
 }: {
-    address: AddressType
+    address: AddressType | null
     deleteAddress?: (addressId: number) => void
     showBtns: boolean
+    onClickHandler?: () => void
 }) {
     const navigate = useNavigate()
     return (
-        <div className="address-card">
+        <div className="address-card" onClick={onClickHandler}>
             <div className="address-info">
-                <div className="name">{address.name}</div>
-                <div className="phone">{address.phone}</div>
-                <div className="address">{address.address}</div>
+                <div className="name">{address ? address.name : ''}</div>
+                <div className="phone">{address ? address.phone : ''}</div>
+                <div className={address?.default ? 'default' : ''}>
+                    {address?.default ? '默认' : ''}
+                </div>
+                <div className="address">{address ? address.address : ''}</div>
             </div>
-            {showBtns && (
+            {showBtns && address && (
                 <div className="edit-btns">
-                    <span className="default">
-                        {address.default ? '默认' : ''}
-                    </span>
-                    <div>
-                        <button
-                            className="edit-btn"
-                            onClick={() =>
-                                navigate(`/new-address/${address.address_id}`)
-                            }
-                        >
-                            编辑
-                        </button>
-                        <button
-                            className="delete-btn"
-                            onClick={() =>
-                                deleteAddress
-                                    ? deleteAddress(address.address_id)
-                                    : null
-                            }
-                        >
-                            删除
-                        </button>
-                    </div>
+                    <button
+                        className="edit-btn"
+                        onClick={() =>
+                            navigate(`/new-address/${address.address_id}`)
+                        }
+                    >
+                        编辑
+                    </button>
+                    <button
+                        className="delete-btn"
+                        onClick={() =>
+                            deleteAddress
+                                ? deleteAddress(address.address_id)
+                                : null
+                        }
+                    >
+                        删除
+                    </button>
                 </div>
             )}
         </div>
