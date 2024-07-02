74a75189934c8c0b3e258346e4760a791377059e
Liang <lianginchina1999@gmail.com>
2024-07-02 17:25:27 +0800

    update popover components

--- src/components/Popover/index.tsx
+++ src/components/Popover/index.tsx
@@ -5,16 +5,18 @@ function Popover({
     show,
     children,
     blankClickCallback,
+    className,
 }: {
     show: boolean
     children: React.ReactNode
     blankClickCallback: () => void
+    className?: string
 }) {
     return show ? (
-        <>
+        <div className={className}>
             <div className="popover-mask" onClick={blankClickCallback}></div>
             <div className="popover-content">{children}</div>
-        </>
+        </div>
     ) : null
 }
 
